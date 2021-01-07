26c9724633274344a45b188fb610a74d2ef51062
Marty Alana <76988481+MartyAlana@users.noreply.github.com>
2021-01-08 07:37:45 +1000

    add unbaked acorn placeholder images

--- src/main/java/me/martyalana/openpixelmon/OpenPixelmon.java
+++ src/main/java/me/martyalana/openpixelmon/OpenPixelmon.java
@@ -2,6 +2,8 @@ package me.martyalana.openpixelmon;
 
 import me.martyalana.openpixelmon.entity.Entities;
 import me.martyalana.openpixelmon.item.OpenPixelmonItems;
+import net.devtech.arrp.api.RRPCallback;
+import net.devtech.arrp.api.RuntimeResourcePack;
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
 import org.apache.logging.log4j.LogManager;
@@ -10,11 +12,14 @@ import org.apache.logging.log4j.Logger;
 public class OpenPixelmon implements ModInitializer {
 
 	public static final Logger LOGGER = LogManager.getLogger("Open Pixelmon");
+	public static final RuntimeResourcePack RESOURCE_PACK = RuntimeResourcePack.create("open_pixelmon");
 
 	@Override
 	public void onInitialize() {
+		LOGGER.info("Open Pixelmon is Initializing");
 		OpenPixelmonItems.initialize();
 		Entities.initialize();
+		RRPCallback.EVENT.register(resources -> resources.add(RESOURCE_PACK));
 		LOGGER.info("Open Pixelmon Initialization successful!");
 	}
 
--- src/main/java/me/martyalana/openpixelmon/item/OpenPixelmonItems.java
+++ src/main/java/me/martyalana/openpixelmon/item/OpenPixelmonItems.java
@@ -2,7 +2,10 @@ package me.martyalana.openpixelmon.item;
 
 import me.martyalana.openpixelmon.OpenPixelmon;
 import me.martyalana.openpixelmon.item.misc.PokeballItem;
+import net.devtech.arrp.json.models.JModel;
+import net.devtech.arrp.json.models.JTextures;
 import net.minecraft.item.Item;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class OpenPixelmonItems {
@@ -18,49 +21,41 @@ public class OpenPixelmonItems {
 	/**
 	 * Acorns
 	 */
-	public static final Item WHITE_ACORN = new Item(new Item.Settings().group(ItemGroups.ORGANIC));
-	public static final Item BlUE_ACORN = new Item(new Item.Settings().group(ItemGroups.ORGANIC));
-	public static final Item YELLOW_ACORN = new Item(new Item.Settings().group(ItemGroups.ORGANIC));
-	public static final Item GREEN_ACORN = new Item(new Item.Settings().group(ItemGroups.ORGANIC));
-	public static final Item PINK_ACORN = new Item(new Item.Settings().group(ItemGroups.ORGANIC));
-	public static final Item RED_ACORN = new Item(new Item.Settings().group(ItemGroups.ORGANIC));
-	public static final Item BLACK_ACORN = new Item(new Item.Settings().group(ItemGroups.ORGANIC));
+	public static final Item WHITE_ACORN = generate(new Item(new Item.Settings().group(ItemGroups.ORGANIC)), "white_acorn");
+	public static final Item BlUE_ACORN = generate(new Item(new Item.Settings().group(ItemGroups.ORGANIC)), "blue_acorn");
+	public static final Item YELLOW_ACORN = generate(new Item(new Item.Settings().group(ItemGroups.ORGANIC)), "yellow_acorn");
+	public static final Item GREEN_ACORN = generate(new Item(new Item.Settings().group(ItemGroups.ORGANIC)), "green_acorn");
+	public static final Item PINK_ACORN = generate(new Item(new Item.Settings().group(ItemGroups.ORGANIC)), "pink_acorn");
+	public static final Item RED_ACORN = generate(new Item(new Item.Settings().group(ItemGroups.ORGANIC)), "red_acorn");
+	public static final Item BLACK_ACORN = generate(new Item(new Item.Settings().group(ItemGroups.ORGANIC)), "black_acorn");
 
-	public static final Item BAKED_WHITE_ACORN = new Item(new Item.Settings().group(ItemGroups.ORGANIC));
-	public static final Item BAKED_BlUE_ACORN = new Item(new Item.Settings().group(ItemGroups.ORGANIC));
-	public static final Item BAKED_YELLOW_ACORN = new Item(new Item.Settings().group(ItemGroups.ORGANIC));
-	public static final Item BAKED_GREEN_ACORN = new Item(new Item.Settings().group(ItemGroups.ORGANIC));
-	public static final Item BAKED_PINK_ACORN = new Item(new Item.Settings().group(ItemGroups.ORGANIC));
-	public static final Item BAKED_RED_ACORN = new Item(new Item.Settings().group(ItemGroups.ORGANIC));
-	public static final Item BAKED_BLACK_ACORN = new Item(new Item.Settings().group(ItemGroups.ORGANIC));
+	public static final Item BAKED_WHITE_ACORN = generate(new Item(new Item.Settings().group(ItemGroups.ORGANIC)), "baked_white_acorn");
+	public static final Item BAKED_BlUE_ACORN = generate(new Item(new Item.Settings().group(ItemGroups.ORGANIC)), "baked_blue_acorn");
+	public static final Item BAKED_YELLOW_ACORN = generate(new Item(new Item.Settings().group(ItemGroups.ORGANIC)), "baked_yellow_acorn");
+	public static final Item BAKED_GREEN_ACORN = generate(new Item(new Item.Settings().group(ItemGroups.ORGANIC)), "baked_green_acorn");
+	public static final Item BAKED_PINK_ACORN = generate(new Item(new Item.Settings().group(ItemGroups.ORGANIC)), "baked_pink_acorn");
+	public static final Item BAKED_RED_ACORN = generate(new Item(new Item.Settings().group(ItemGroups.ORGANIC)), "baked_red_acorn");
+	public static final Item BAKED_BLACK_ACORN = generate(new Item(new Item.Settings().group(ItemGroups.ORGANIC)), "baked_black_acorn");
 
 	public static void initialize() {
-		registerPokeballs();
-		registerAcorns();
 	}
 
-	private static void registerAcorns() {
-		Registry.register(Registry.ITEM, OpenPixelmon.id("white_acorn"), WHITE_ACORN);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("blue_acorn"), BlUE_ACORN);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("yellow_acorn"), YELLOW_ACORN);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("green_acorn"), GREEN_ACORN);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("pink_acorn"), PINK_ACORN);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("red_acorn"), RED_ACORN);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("black_acorn"), BLACK_ACORN);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("baked_white_acorn"), WHITE_ACORN);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("baked_blue_acorn"), BlUE_ACORN);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("baked_yellow_acorn"), YELLOW_ACORN);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("baked_green_acorn"), GREEN_ACORN);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("baked_pink_acorn"), PINK_ACORN);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("baked_red_acorn"), RED_ACORN);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("baked_black_acorn"), BLACK_ACORN);
-	}
-
-	private static void registerPokeballs() {
-		Registry.register(Registry.ITEM, OpenPixelmon.id("pokeball"), POKEBALL);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("greatball"), GREATBALL);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("ultraball"), ULTRABALL);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("masterball"), MASTERBALL);
-		Registry.register(Registry.ITEM, OpenPixelmon.id("quickball"), QUICKBALL);
+	/**
+	 * Used to generate a basic item model
+	 *
+	 * @param item the item to generate the model for
+	 * @param id   the id of the item
+	 * @return the item that was passed in
+	 */
+	public static Item generate(Item item, String id) {
+		Identifier identifier = OpenPixelmon.id(id);
+		Identifier modelIdentifier = OpenPixelmon.id("item/" + id);
+		OpenPixelmon.RESOURCE_PACK.addModel(new JModel()
+						.parent("item/generated")
+						.textures(new JTextures()
+								.layer0("open_pixelmon:item/" + id)),
+				modelIdentifier);
+		Registry.register(Registry.ITEM, identifier, item);
+		return item;
 	}
 }
