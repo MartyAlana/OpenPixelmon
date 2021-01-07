4dabff3b0fc2ca86ba892d2819542e0a76b5dba3
Marty Alana <76988481+MartyAlana@users.noreply.github.com>
2021-01-07 16:44:11 +1000

    Setup Project

---
+++ src/main/java/me/martyalana/openpixelmon/Openpixelmon.java
@@ -0,0 +1,10 @@
+package me.martyalana.openpixelmon;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Openpixelmon implements ModInitializer {
+	@Override
+	public void onInitialize() {
+
+	}
+}
