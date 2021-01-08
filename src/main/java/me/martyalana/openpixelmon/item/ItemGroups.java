53c4d56deebf7cbf2cc8cad283efe606d9f99ae9
Marty Alana <76988481+MartyAlana@users.noreply.github.com>
2021-01-08 15:48:13 +1000

    prep for apricorn retexture

--- src/main/java/me/martyalana/openpixelmon/item/ItemGroups.java
+++ src/main/java/me/martyalana/openpixelmon/item/ItemGroups.java
@@ -11,8 +11,38 @@ public class ItemGroups {
 			() -> new ItemStack(OpenPixelmonItems.POKE_BALL)
 	);
 
+	public static final ItemGroup RESTORATION = FabricItemGroupBuilder.build(
+			OpenPixelmon.id("restoration"),
+			() -> new ItemStack(OpenPixelmonItems.POKE_BALL)
+	);
+
 	public static final ItemGroup ORGANIC = FabricItemGroupBuilder.build(
 			OpenPixelmon.id("organic"),
 			() -> new ItemStack(OpenPixelmonItems.RED_ACORN)
+	);
+
+	public static final ItemGroup TMS = FabricItemGroupBuilder.build(
+			OpenPixelmon.id("tms"),
+			() -> new ItemStack(OpenPixelmonItems.POKE_BALL)
+	);
+
+	public static final ItemGroup BLOCKS = FabricItemGroupBuilder.build(
+			OpenPixelmon.id("blocks"),
+			() -> new ItemStack(OpenPixelmonItems.POKE_BALL)
+	);
+
+	public static final ItemGroup ITEMS = FabricItemGroupBuilder.build(
+			OpenPixelmon.id("items"),
+			() -> new ItemStack(OpenPixelmonItems.POKE_BALL)
+	);
+
+	public static final ItemGroup BADGES = FabricItemGroupBuilder.build(
+			OpenPixelmon.id("badges"),
+			() -> new ItemStack(OpenPixelmonItems.POKE_BALL)
+	);
+
+	public static final ItemGroup DECORATION = FabricItemGroupBuilder.build(
+			OpenPixelmon.id("decoration"),
+			() -> new ItemStack(OpenPixelmonItems.POKE_BALL)
 	);
 }
