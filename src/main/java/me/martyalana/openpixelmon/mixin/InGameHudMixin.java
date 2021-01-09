a2efb5c24ce2df50a9fa01f3985b69ead13a2b64
Marty Alana <76988481+MartyAlana@users.noreply.github.com>
2021-01-09 19:59:57 +1000

    Prepare for sidebar party.

--- src/main/java/me/martyalana/openpixelmon/mixin/InGameHudMixin.java
+++ src/main/java/me/martyalana/openpixelmon/mixin/InGameHudMixin.java
@@ -21,10 +21,19 @@ public class InGameHudMixin extends DrawableHelper {
 	@Shadow @Final private MinecraftClient client;
 
 	private static final Identifier SIDEBAR = OpenPixelmon.id("textures/gui/ingame/sidebar.png");
+	private static final Identifier POKEBALL = OpenPixelmon.id("textures/gui/ingame/pokemon.png");
 
 	@Inject(method = "renderHotbar", at = @At("HEAD"))
 	private void renderPixelmonParty(float tickDelta, MatrixStack matrices, CallbackInfo ci) {
 		client.getTextureManager().bindTexture(SIDEBAR);
-		this.drawTexture(matrices, 0, this.scaledHeight / 2 - 83, 0, 0, 36, 167);
+		this.drawTexture(matrices, 0, this.scaledHeight / 2 - (167/2), 0, 0, 36, 167);
+
+		client.getTextureManager().bindTexture(POKEBALL);
+		drawTexture(matrices, 0, this.scaledHeight / 2 - (22/2) - 65, 0, 0, 22, 22, 22, 22);
+		drawTexture(matrices, 0, this.scaledHeight / 2 - (22/2) - 37, 0, 0, 22, 22, 22, 22);
+		drawTexture(matrices, 0, this.scaledHeight / 2 - (22/2) - 9, 0, 0, 22, 22, 22, 22);
+		drawTexture(matrices, 0, this.scaledHeight / 2 - (22/2) + 19, 0, 0, 22, 22, 22, 22);
+		drawTexture(matrices, 0, this.scaledHeight / 2 - (22/2) + 47, 0, 0, 22, 22, 22, 22);
+		drawTexture(matrices, 0, this.scaledHeight / 2 - (22/2) + 73, 0, 0, 22, 22, 22, 22);
 	}
 }
