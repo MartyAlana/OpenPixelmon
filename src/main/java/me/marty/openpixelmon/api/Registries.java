276bddeea86616d1818a9070f63e17f37c2dc5d9
Ramid Khan <ramidzkh@gmail.com>
2021-01-13 12:36:37 +1100

    Add to builtin registries

--- src/main/java/me/marty/openpixelmon/api/Registries.java
+++ src/main/java/me/marty/openpixelmon/api/Registries.java
@@ -6,6 +6,7 @@ import me.marty.openpixelmon.api.pixelmon.PokedexEntry;
 import me.marty.openpixelmon.api.pixelmon.move.Move;
 import net.fabricmc.fabric.api.event.registry.DynamicRegistrySetupCallback;
 import net.fabricmc.fabric.api.event.registry.RegistryEntryAddedCallback;
+import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
 
@@ -15,6 +16,9 @@ public class Registries {
 	public static final RegistryKey<Registry<Move>> MOVE_REGISTRY = RegistryKey.ofRegistry(OpenPixelmon.id("move"));
 
 	public static void register() {
+		BuiltinRegistries.addRegistry(PIXELMON_REGISTRY, () -> null);
+		BuiltinRegistries.addRegistry(MOVE_REGISTRY, () -> null);
+
 		DynamicRegistryBuilderCallback.EVENT.register(builder -> {
 			builder.register(PIXELMON_REGISTRY, PokedexEntry.CODEC);
 			builder.register(MOVE_REGISTRY, Move.CODEC);
