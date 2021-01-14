c16e51ea7438056d919a569cb77ffc63d255eb26
Marty Alana <76988481+MartyAlana@users.noreply.github.com>
2021-01-14 20:32:28 +1000

    Add ambient 4

--- src/main/java/me/marty/openpixelmon/sound/Sounds.java
+++ src/main/java/me/marty/openpixelmon/sound/Sounds.java
@@ -10,15 +10,21 @@ import net.minecraft.util.registry.Registry;
 public class Sounds {
 
 	public static final SoundEvent BATTLE_1 = registerSound("music.battle_1");
+	public static final SoundEvent BATTLE_2 = registerSound("music.battle_2");
 
 	public static final SoundEvent AMBIENT_1 = registerSound("music.ambient_1");
 	public static final SoundEvent AMBIENT_2 = registerSound("music.ambient_2");
+	public static final SoundEvent AMBIENT_4 = registerSound("music.ambient_4");
 
 	public static final SoundEvent MENU = registerSound("music.menu");
 
-	public static final MusicSound BATTLE_1_LOOP = MusicType.createIngameMusic(BATTLE_1);
 	public static final MusicSound AMBIENT_1_LOOP = MusicType.createIngameMusic(AMBIENT_1);
 	public static final MusicSound AMBIENT_2_LOOP = MusicType.createIngameMusic(AMBIENT_2);
+	public static final MusicSound AMBIENT_4_LOOP = MusicType.createIngameMusic(AMBIENT_4);
+
+	public static final MusicSound BATTLE_1_LOOP = MusicType.createIngameMusic(BATTLE_1);
+	public static final MusicSound BATTLE_2_LOOP = MusicType.createIngameMusic(BATTLE_2);
+
 	public static final MusicSound MENU_LOOP = MusicType.createIngameMusic(MENU);
 
 	private static SoundEvent registerSound(String id) {
